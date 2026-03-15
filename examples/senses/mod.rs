be2b2ed0159a47177b752672452914192a46faca
冯帅真 <f17791931304@163.com>
2026-03-15 15:24:08 +0800

    add llm examples

--- examples/senses/mod.rs
+++ examples/senses/mod.rs
@@ -1,11 +1,9 @@
-pub(crate) mod cup;
 pub(crate) mod militech_acs;
 pub(crate) mod llm_a;
 pub(crate) mod llm_b;
 
 #[allow(dead_code)]
 pub struct Senses {
-    pub cup: cup::CupSense,
     pub militech_acs: militech_acs::ACSSense,
     pub llm_a: llm_a::LLMASense,
     pub llm_b: llm_b::LLMBSense,
@@ -14,7 +12,6 @@ pub struct Senses {
 impl Senses {
     pub fn new() -> Self {
         Self {
-            cup: cup::CupSense::new(),
             militech_acs: militech_acs::ACSSense::new(),
             llm_a: llm_a::LLMASense::new(),
             llm_b: llm_b::LLMBSense::new(),
